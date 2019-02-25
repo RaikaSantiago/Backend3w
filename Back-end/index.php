1a29a6e857ab3ac5f1e286c848915d156f878eeb
Santiago981 <santiago.rivera@correounivalle.edu.co>
2019-02-24 21:09:58 -0500

    Update index.php

--- Back-end/index.php
+++ Back-end/index.php
@@ -18,8 +18,8 @@ $app->get("/usuarios", function() use($app, $db){
 	$query = $db->query($sql);
 
 	$usuarios = array();
-	while($usuarios = $query->fetch_assoc()){
-		$usuarios[] = $usuarios;
+	while($usuario = $query->fetch_assoc()){
+		$usuarios[] = $usuario;
 	}
 
 	$result = array('status' => 'success',
@@ -30,14 +30,15 @@ $app->get("/usuarios", function() use($app, $db){
 
 
 });
+
 $app->post("/usuarios", function() use($app, $db){
 
 	
-	$json = $request->post('json');
+	$json = $request->getParam('json');
 	$data = json_decode($json, true);
 
 	$query = "INSERT INTO usuarios VALUES(NULL,".
-		"'{$data['id_Usuarios']}',".
+		"'{$data['Id_Usuarios']}',".
 		"'{$data['Usuario']}',".
 		"'{$data['Clave']}'";
 
@@ -53,6 +54,163 @@ $app->post("/usuarios", function() use($app, $db){
 		$result = array('status' => 'success',
 						'code' => 200,
 						'message' => 'Usuario creado correctamente'.$insert);
+	}
+
+	echo json_encode($result);
+
+});
+
+$app->post("/roles", function() use($app, $db){
+
+	
+	$json = $request->getParam('json');
+	$data = json_decode($json, true);
+
+	$query = "INSERT INTO roles VALUES(NULL,".
+		"'{$data['idRoles']}',".
+		"'{$data['Usuarios_Id_Usuario']}',".
+		"'{$data['NombreRol']}'";
+
+
+	$insert = $db->query($query);
+
+
+	$result = array('status' => 'error',
+						'code' => 150,
+						'message' => 'Error al crear el rol, detalle del error error=['.$db->error.']');
+
+	if($insert){
+		$result = array('status' => 'success',
+						'code' => 200,
+						'message' => 'Rol  creado correctamente'.$insert);
+	}
+
+	echo json_encode($result);
+
+});
+
+$app->post("/innovacion", function() use($app, $db){
+
+	
+	$json = $request->getParam('json');
+	$data = json_decode($json, true);
+
+	$query = "INSERT INTO innovacion VALUES(NULL,".
+		"'{$data['Id_Innovacion']}',".
+		"'{$data['Id_Usuario']}',".
+		"'{$data['NombreObjetivo']}',".
+		"'{$data['Indicador']}',".
+		"'{$data['Meta']}',".
+		"'{$data['Iniciativa']}'";
+
+
+	$insert = $db->query($query);
+
+
+	$result = array('status' => 'error',
+						'code' => 150,
+						'message' => 'Error al crear innovacion, detalle del error error=['.$db->error.']');
+
+	if($insert){
+		$result = array('status' => 'success',
+						'code' => 200,
+						'message' => 'Innovacion creada correctamente'.$insert);
+	}
+
+	echo json_encode($result);
+
+});
+
+$app->post("/gestionhumana", function() use($app, $db){
+
+	
+	$json = $request->getParam('json');
+	$data = json_decode($json, true);
+
+	$query = "INSERT INTO gestionhumana VALUES(NULL,".
+		"'{$data['ID_GestionHumana']}',".
+		"'{$data['Id_Usuario']}',".
+		"'{$data['NombreObjetivo']}',".
+		"'{$data['Indicador']}',".
+		"'{$data['Meta']}',".
+		"'{$data['Iniciativa']}'";
+
+
+	$insert = $db->query($query);
+
+
+	$result = array('status' => 'error',
+						'code' => 150,
+						'message' => 'Error al crear Gestion Humana, detalle del error error=['.$db->error.']');
+
+	if($insert){
+		$result = array('status' => 'success',
+						'code' => 200,
+						'message' => 'Gestion Humana creada correctamente'.$insert);
+	}
+
+	echo json_encode($result);
+
+});
+
+$app->post("/financiera", function() use($app, $db){
+
+	
+	$json = $request->getParam('json');
+	$data = json_decode($json, true);
+
+	$query = "INSERT INTO financiera VALUES(NULL,".
+		"'{$data['Id_Financiera']}',".
+		"'{$data['Id_Usuario']}',".
+		"'{$data['NombreObjetivo']}',".
+		"'{$data['Indicador']}',".
+		"'{$data['Meta']}',".
+		"'{$data['Iniciativa']}'";
+
+
+	$insert = $db->query($query);
+
+
+	$result = array('status' => 'error',
+						'code' => 150,
+						'message' => 'Error al crear Financiera, detalle del error error=['.$db->error.']');
+
+	if($insert){
+		$result = array('status' => 'success',
+						'code' => 200,
+						'message' => 'Financiera creada correctamente'.$insert);
+	}
+
+	echo json_encode($result);
+
+});
+
+$app->post("/cliente", function() use($app, $db){
+
+	
+	$json = $request->getParam('json');
+	$data = json_decode($json, true);
+
+	$query = "INSERT INTO cliente VALUES(NULL,".
+		"'{$data['Id_Cliente']}',".
+		"'{$data['Id_Usuario']}',".
+		"'{$data['NombreObjetivo']}',".
+		"'{$data['Indicador']}',".
+		"'{$data['Meta']}',".
+		"'{$data['Iniciativa']}'";
+
+
+	$insert = $db->query($query);
+
+
+	$result = array('status' => 'error',
+						'code' => 150,
+						'message' => 'Error al crear Cliente, detalle del error error=['.$db->error.']');
+
+	if($insert){
+		$result = array('status' => 'success',
+						'code' => 200,
+						'message' => 'Cliente creada correctamente'.$insert);
 	}
 
 	echo json_encode($result);
